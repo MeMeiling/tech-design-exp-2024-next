c81bca611b2f77229f2b6d9f78208a6fce11fbed
Mei <pitcha@5lab.co>
2024-03-27 01:53:39 +0700

    homework

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="min-h-[100vh] justify-center flex flex-col item-center">
+            <h1 className="pl-12 font-light py-2">Find your</h1>
+            <h1 className="pl-12 py-2">activity</h1>
+        </div>
+    );
+}
